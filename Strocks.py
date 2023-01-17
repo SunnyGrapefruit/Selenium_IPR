1d8501b8e9bc5b87e254019e9e1f94e70bb1c3dc
SunnyGrapefruit <wir_die_traumer@mail.ru>
2023-01-17 19:30:24 +0500

    IPR

--- Strocks.py
+++ Strocks.py
@@ -2,11 +2,13 @@ import json
 from json import JSONEncoder
 
 from selenium import webdriver
+import time
+from selenium.webdriver.common.action_chains import ActionChains
 
-# options = webdriver.ChromeOptions()
+# options = ChromeOptions()
 # chrome_driver_binary = 'C:\chromedriver\chromedriver.exe'
 driver = webdriver.Chrome()
-
+action = webdriver.ActionChains(driver)
 
 class Stocks:
     def __init__(self, id, name, price, link):
@@ -34,8 +36,21 @@ class Stocks:
 #     def default(self, o):
 #         return o.__dict__
 
-link = "https://ru.investing.com/equities/russia"
+# link = "https://ru.investing.com/equities/russia"
+
+link = "https://ru.investing.com"
 driver.get(link)
+
+element_to_hover_over_1 = driver.find_element_by_xpath("//*[@id='navMenu']/ul/li[1]/a")
+hover_1 = action.move_to_element(element_to_hover_over_1)
+hover_1.perform()
+
+element_to_hover_over_2 = driver.find_element_by_xpath("//*[@id='navMenu']/ul/li[1]/ul/li[4]/a")
+hover_2 = action.move_to_element(element_to_hover_over_2)
+hover_2.perform()
+
+driver.find_element_by_xpath("//*[@id='navMenu']/ul/li[1]/ul/li[4]/div/ul[1]/li[3]/a").click()
+
 all_stock = list()
 # all_stock_dict = dict()
 root_element = driver.find_elements_by_xpath(f'//*[@id="cross_rate_markets_stocks_1"]/tbody/tr')
@@ -44,7 +59,6 @@ for i in range(5): #len(root_element)
     curr_element = root_element[i]
     stock = Stocks.pars(curr_element)
     all_stock.append(stock)
-
 for stock in all_stock:
     driver.get(stock.link)
     try:
@@ -53,6 +67,14 @@ for stock in all_stock:
         dividends = 0
     stock.dividends = (float(dividends)/float(stock.price))*100
     # all_stock_dict[stock.id] = [stock.name, stock.dividends]
+
+stocks_json = json.dumps(all_stock, indent=4, default=Stocks.obj_to_dict)
+with open("stocks.json", "w") as my_file:
+    my_file.write(stocks_json)
+print('Запись завершена')
+
+driver.quit()
+
 
 # stocks_json = json.dumps(all_stock_dict, indent=4)
 # with open("stocks.json", "w") as my_file:
@@ -63,10 +85,3 @@ for stock in all_stock:
 # with open("stocks.json", "w") as my_file:
 #     my_file.write(stocks_json)
 # print('Запись завершена')
-#
-stocks_json = json.dumps(all_stock, indent=4, default=Stocks.obj_to_dict)
-with open("stocks.json", "w") as my_file:
-    my_file.write(stocks_json)
-print('Запись завершена')
-
-driver.quit()
